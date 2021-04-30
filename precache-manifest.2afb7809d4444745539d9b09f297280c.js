935cd818d063534af248cbc5a051dcbfc511b245
Carl Karlsson Schmidt <carl.karlsson.schmidt@niradynamics.se>
2021-04-30 15:44:02 +0200

    first commit

---
+++ precache-manifest.2afb7809d4444745539d9b09f297280c.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250d3efe9e1470a53bf16ecc0126ada6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69e0b8ca9a508edb3d1",
+    "url": "/static/css/2.2b029df0.chunk.css"
+  },
+  {
+    "revision": "d4fd1a7446b1848c302e",
+    "url": "/static/css/main.df72d588.chunk.css"
+  },
+  {
+    "revision": "d69e0b8ca9a508edb3d1",
+    "url": "/static/js/2.2cf6b652.chunk.js"
+  },
+  {
+    "revision": "d4fd1a7446b1848c302e",
+    "url": "/static/js/main.978f4d68.chunk.js"
+  },
+  {
+    "revision": "79bf7190b66126ac4480",
+    "url": "/static/js/runtime-main.ca187ff8.js"
+  },
+  {
+    "revision": "3fcaca9549431b151fd0bf8d0396bffd",
+    "url": "/static/media/1.3fcaca95.jpg"
+  },
+  {
+    "revision": "14b5a2601fc78915ef7c4600d917e7e8",
+    "url": "/static/media/2.14b5a260.jpg"
+  },
+  {
+    "revision": "8dfce1a3c2659bed44d6291afdfd37e9",
+    "url": "/static/media/3.8dfce1a3.jpg"
+  },
+  {
+    "revision": "dda696d84b260c015fee086cbbb91a69",
+    "url": "/static/media/4.dda696d8.jpg"
+  },
+  {
+    "revision": "17a0c5af6825e73098637f2915d97d0a",
+    "url": "/static/media/5.17a0c5af.jpg"
+  },
+  {
+    "revision": "d1763a3132f2dce39a46cae0d039a2c0",
+    "url": "/static/media/components.d1763a31.svg"
+  },
+  {
+    "revision": "6369c7175ace050a43c32ed3cbd25e53",
+    "url": "/static/media/dashboard.6369c717.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/glyphicons-halflings-regular.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/glyphicons-halflings-regular.d41d8cd9.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "d843562e57d02cef2a2a502c555e9c40",
+    "url": "/static/media/logo.d843562e.svg"
+  },
+  {
+    "revision": "55b5dc6329f1024e47aa8b230d293d2b",
+    "url": "/static/media/mail.55b5dc63.svg"
+  },
+  {
+    "revision": "af8499158ac85c5f7c0b5f0da0e192cc",
+    "url": "/static/media/notification.af849915.svg"
+  },
+  {
+    "revision": "96e2098a78f4defccb79c270f58d358c",
+    "url": "/static/media/notifications.96e2098a.svg"
+  },
+  {
+    "revision": "cabfce3fddb9a700b239c034601f3bc4",
+    "url": "/static/media/photo.cabfce3f.jpg"
+  },
+  {
+    "revision": "beeeb69dc6051ab52ec2d697894bc8b6",
+    "url": "/static/media/settings.beeeb69d.svg"
+  },
+  {
+    "revision": "9687282b2e9d1fc1187edddb703b25df",
+    "url": "/static/media/tables.9687282b.svg"
+  },
+  {
+    "revision": "af74b5506aa9ffc26511f5cb803d5fe1",
+    "url": "/static/media/typography.af74b550.svg"
+  }
+]);
